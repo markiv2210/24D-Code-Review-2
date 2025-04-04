beee89edf85ce72a4ba9f7bd698f2fa8239de28b
vikram.ramachandriya <vikram.ramachandriya@arrise.com>
2025-04-04 16:15:18 +0530

    betboard changes

--- src/components/betBoard/betBoard.js
+++ src/components/betBoard/betBoard.js
@@ -1,114 +1,114 @@
 import './betBoard.css';
 
 export default function BetBoard() {
-  return (
-    <div class="betboard">
-    <div class="transparentBlackBackground borderTopLeft rangeCells topLeftEmptySpace borderTR borderL">1-4</div>
-    <div class="transparentBlackBackground rangeCells borderTR">5-8</div>
-    <div class="transparentBlackBackground rangeCells borderTR">9-12</div>
-    <div class="transparentBlackBackground rangeCells borderTR">13-16</div>
-    <div class="transparentBlackBackground rangeCells borderTR">17-20</div>
-    <div class="transparentBlackBackground borderTopRight rangeCells borderTR">21-24</div>
+    return (
+        <div class="betboardParent">
+            <div class="betboard">
+                <div class="rangeOfBets">
+                    <div class="parentForTopRangeCells">
+                        <div class="borderTR borderL borderproperties fontSizeFourteen borderTopLeft bgColorBlack">1-4
+                        </div>
+                        <div class="borderTR borderproperties fontSizeFourteen bgColorBlack">5-8</div>
+                        <div class="borderTR borderproperties fontSizeFourteen bgColorBlack">9-12</div>
+                        <div class="borderTR borderproperties fontSizeFourteen bgColorBlack">13-16</div>
+                        <div class="borderTR borderproperties fontSizeFourteen bgColorBlack">17-20</div>
+                        <div class="borderTR borderproperties fontSizeFourteen borderTopRight bgColorBlack">21-24</div>
+                    </div>
+                    <div class="thinline borderTR">4.75:1</div>
+                </div>
 
+                <div class="leftMarkers">
+                    <div class="cell borderL borderTR col  borderTopLeft bgColorBlack">
+                        <div class="fontSizeFourteen">1st</div>
+                        <div class="fontSizeTwelve">row</div>
+                    </div>
+                    <div class="cell borderL borderTR col bgColorBlack">
+                        <div class="fontSizeFourteen">2nd</div>
+                        <div class="fontSizeTwelve">row</div>
+                    </div>
+                    <div class="cell borderL borderTR col bgColorBlack">
+                        <div class="fontSizeFourteen">3rd</div>
+                        <div class="fontSizeTwelve">row</div>
+                    </div>
+                    <div class="cell borderL borderTR col bgColorBlack">
+                        <div class="fontSizeFourteen">4th</div>
+                        <div class="fontSizeTwelve">row</div>
+                    </div>
+                    <div class="thinline borderL borderTR borderD"> 2.84:1</div>
+                </div>
 
-    <div class="thinMiddleLineSpan thin tRB lBorder dBorder borderTR borderL borderD thinTopAndMiddle fontSizeTen">4.75:1
-    </div>
+                <div class="mainBets">
+                    <div class="mainRed borderTR borderL">1</div>
+                    <div class="mainBlack borderTR">5</div>
+                    <div class="mainRed  borderTR">9</div>
+                    <div class="mainBlack borderTR">13</div>
+                    <div class="mainRed borderTR">17</div>
+                    <div class="mainBlack borderTR">21</div>
+                    <div class="mainRed borderTR borderL">2</div>
+                    <div class="mainBlack borderTR">6</div>
+                    <div class="mainRed borderTR">10</div>
+                    <div class="mainBlack borderTR">14</div>
+                    <div class="mainRed borderTR">18</div>
+                    <div class="mainBlack borderTR">22</div>
+                    <div class="mainRed borderTR borderL">3</div>
+                    <div class="mainBlack borderTR">7</div>
+                    <div class="mainRed borderTR">11</div>
+                    <div class="mainBlack borderTR">15</div>
+                    <div class="mainRed borderTR">19</div>
+                    <div class="mainBlack borderTR">23</div>
+                    <div class="mainRed borderTR borderL">4</div>
+                    <div class="mainBlack borderTR">8</div>
+                    <div class="mainRed borderTR">12</div>
+                    <div class="mainBlack borderTR">16</div>
+                    <div class="mainRed borderTR">20</div>
+                    <div class="mainBlack borderTR">24</div>
+                    <div class="thinline borderTR mainThinLine borderL borderD">22:1</div>
+                </div>
 
-    <div
-        class="marginright transparentBlackBackground rowMarker mainBetCellIndiRowMarker borderTopLeft tRB lBorder topRightEmptySpace borderTR borderL">
-        <div class="rowMarkerParentDiv">
-            <div class="numberSTStyle">1st</div>
-            <div class="rowStyle">row</div>
-        </div>
-    </div>
+                <div class="rightMarker">
+                    <div class="cell borderTR borderL fontSizeFourteen borderTopRight bgColorBlack"> 1st&<br />2nd</div>
+                    <div class="cell borderTR borderL fontSizeFourteen bgColorBlack"> 3rd&<br />4th</div>
+                    <div class="thinline  borderTR borderD borderL"> 0.95:1</div>
+                </div>
 
-    <div class="redMainBet borderTR borderL">1</div>
-    <div class="blackMainBet borderTR ">5</div>
-    <div class="redMainBet borderTR ">9</div>
-    <div class="blackMainBet borderTR ">13</div>
-    <div class="redMainBet borderTR ">17</div>
-    <div class="blackMainBet borderTR ">21</div>
+                <div class="leftBottom">
+                    <div class="borderTR borderL col bgColorBlack flexGenProp">
+                        <div class="fontSizeFourteen">Small</div>
+                        <div class="fontSizeTen lineHeightFourteen">0.95:1</div>
+                    </div>
+                    <div class="borderTR  borderL  borderBottomLeft borderD col bgColorBlack flexGenProp">
+                        <div class="fontSizeFourteen">Odd</div>
+                        <div class="fontSizeTen lineHeightFourteen">0.95:1</div>
+                    </div>
+                </div>
 
-    <div class="mix m1 marginleft transparentBlackBackground rowMarker borderTopRight borderTR borderL">
-        1st&<br/>2nd</div>
+                <div class="bottomMiddleUpper">
+                    <div class="parentForBottomRangeCells">
+                        <div class=" borderTR borderproperties borderL fontSizeFourteen bgColorBlack">1-8</div>
+                        <div class="borderTR borderproperties fontSizeFourteen bgColorBlack">5-12</div>
+                        <div class="borderTR borderproperties fontSizeFourteen bgColorBlack">9-16</div>
+                        <div class="borderTR borderproperties fontSizeFourteen bgColorBlack">13-20</div>
+                        <div class="borderTR borderproperties fontSizeFourteen bgColorBlack">17-24</div>
+                    </div>
+                    <div class="thinline borderD borderL borderTR">4.75:1</div>
+                </div>
 
-    <div class="marginright transparentBlackBackground rowMarker mainBetCellIndiRowMarker borderTR borderL">
-        <div class="rowMarkerParentDiv">
-            <div class="numberSTStyle">2nd</div>
-            <div class="rowStyle">row</div>
-        </div>
-    </div>
-    <div class="redMainBet  borderTR borderL ">2</div>
-    <div class="blackMainBet borderTR ">6</div>
-    <div class="redMainBet  borderTR ">10</div>
-    <div class="blackMainBet  borderTR ">14</div>
-    <div class="redMainBet borderTR ">18</div>
-    <div class="blackMainBet borderTR ">22</div>
-    <div class="marginright transparentBlackBackground rowMarker mainBetCellIndiRowMarker borderTR borderL">
-        <div class="rowMarkerParentDiv">
-            <div class="numberSTStyle">3rd</div>
-            <div class="rowStyle">row</div>
-        </div>
-    </div>
-    <div class="redMainBet borderTR borderL ">3</div>
-    <div class="blackMainBet borderTR ">7</div>
-    <div class="redMainBet borderTR ">11</div>
-    <div class="blackMainBet borderTR ">15</div>
-    <div class="redMainBet borderTR ">19</div>
-    <div class="blackMainBet borderTR ">23</div>
-    <div class="mix m2 marginleft transparentBlackBackground  rowMarker  borderTR  borderL">3rd&<br/>4th</div>
-    <div class="marginright transparentBlackBackground rowMarker mainBetCellIndiRowMarker borderTR borderL">
-        <div class="rowMarkerParentDiv">
-            <div class="numberSTStyle">4th</div>
-            <div class="rowStyle">row</div>
-        </div>
-    </div>
-    <div class="redMainBet borderTR borderL">4</div>
-    <div class="blackMainBet borderTR ">8</div>
-    <div class="redMainBet borderTR ">12</div>
-    <div class="blackMainBet borderTR ">16</div>
-    <div class="redMainBet borderTR ">20</div>
-    <div class="blackMainBet borderTR ">24</div>
-    <div class="marginright thin borderTR borderL borderD thinMidLeftAndRight fontSizeTen">2.84:1</div>
-    <div class="thinMiddleLineSpan thin borderTR borderL borderD thinTopAndMiddle fontSizeTen">22:1</div>
-    <div class="marginleft thin borderTR borderL borderD thinMidLeftAndRight fontSizeTen">0.95:1</div>
+                <div class="bottomRight">
+                    <div class="borderTR borderL  bgColorBlack col flexGenProp">
+                        <div class="fontSizeFourteen">Big</div>
+                        <div class="fontSizeTen lineHeightFourteen">0.95:1</div>
+                    </div>
+                    <div class="borderTR borderL borderD col bgColorBlack borderBottomRight flexGenProp">
+                        <div class="fontSizeFourteen">Even</div>
+                        <div class="fontSizeTen lineHeightFourteen">0.95:1</div>
+                    </div>
+                </div>
 
-    <div class="gridfordown">
-        <div class="small marginright transparentBlackBackground  borderTR  borderL">
-            <div class="SBOE">
-                <div class="naming">Small</div>
-                <div class="fontSizeTen">0.95:1</div>
+                <div class="bottomMiddleLower">
+                    <div class="lowerRed borderTR borderL borderD fontSizeTen">0.95:1</div>
+                    <div class="lowerBlack borderTR borderD fontSizeTen">0.95:1</div>
+                </div>
             </div>
         </div>
-        <div class="transparentBlackBackground rangeCells  borderTR borderL">1-8</div>
-        <div class="transparentBlackBackground rangeCells borderTR ">5-12</div>
-        <div class="transparentBlackBackground rangeCells borderTR ">9-16</div>
-        <div class="transparentBlackBackground rangeCells borderTR ">13-20</div>
-        <div class="marginright transparentBlackBackground rangeCells borderTR ">17-24</div>
-        <div class="big transparentBlackBackground borderTR borderL ">
-            <div class="SBOE">
-                <div class="naming">Big</div>
-                <div class="fontSizeTen">0.95:1</div>
-            </div>
-        </div>
-        <div class="middleline marginright thin borderTR  borderL  borderD thinBottom fontSizeTen">4.75:1</div>
-        <div class="thirdline marginright transparentBlackBackground borderBottomLeft borderTR borderL borderD ">
-            <div class="SBOE">
-                <div class="naming">Odd</div>
-                <div class="fontSizeTen">0.95:1</div>
-            </div>
-        </div>
-        <div class="redblackparent marginright">
-            <div class="redthird borderTR borderL borderD fontSizeTen">0.95:1</div>
-            <div class="blackthird borderTR borderD fontSizeTen">0.95:1</div>
-        </div>
-        <div class="thirdline transparentBlackBackground borderBottomRight borderTR  borderL borderD ">
-            <div class="SBOE">
-                <div class="naming">Even</div>
-                <div class="fontSizeTen">0.95:1</div>
-            </div>
-        </div>
-    </div>
-  </div>
-  );
+    );
 }
